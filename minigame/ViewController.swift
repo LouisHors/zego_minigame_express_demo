0c7e18f986fa97e2a63a88e1b17855b3e8dc2e12
liuhao <horsliu@zego.im>
2024-01-04 12:02:41 +0800

    Initial Commit

---
+++ minigame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minigame
+//
+//  Created by zego on 2024/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

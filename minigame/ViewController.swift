e259dc4fc2eb59de48cf353d8a32275b774741ea
liuhao <horsliu@zego.im>
2024-03-07 19:55:49 +0800

    add: add main files

--- minigame/ViewController.swift
+++ minigame/ViewController.swift
@@ -4,44 +4,69 @@
 //
 //  Created by zego on 2024/1/4.
 //
-import SwiftUI
 
-struct ContentView: View {
-    var headers = ["棋牌类", "射击类", "其他"]
-    let items: [String] = ["掼蛋", "射击大师", "敬请期待..."]
-    @State private var singleSelection: UUID?
+import UIKit
+import ZegoExpressEngine
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var gameList: UITableView!
     
-    var body: some View {
-        NavigationView {
-            List(selection: $singleSelection) {
-                ForEach (headers, id: \.self) {header in
-                    Section(header: Text(header)) {
-                        Button {
-                            onSelectGame(idx: headers.firstIndex(of: header) ?? 0)
-                        } label: {
-                            Text(items[headers.firstIndex(of: header) ?? 0])
-                                .labelStyle(.titleOnly)
-                                .foregroundColor(.black)
-                        }
-                    }
-                }
-            }
-            .navigationBarTitle("Game List")
-        }
+    let gameNameList: [String] = ["打扑克", "打飞机", "打咩"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gameList.dataSource = self
+        gameList.delegate = self
+        createZego()
+        loginRoom()
     }
     
-    func onSelectGame(idx: Int) {
-        // 跳转
-        if idx == 2 {
-            // alert
+    func requestGameList() {
+        
+    }
+    
+    func createZego() {
+        ZegoRTCManager.shared.initRTC()
+    }
+    
+    func loginRoom() {
+        ZegoRTCManager.shared.loginRoom(roomID: "aaa", userID: "bbb") { errorCode in
             
         }
     }
+    
+    func onSelectGame(index: Int) {
+        // 点击了具体的游戏,跳转到具体的游戏页
+        switch index {
+        case 0, 1:
+            // 都跳转到同一个页面,只是加载不同的游戏
+            
+            return
+        default:
+            let alertVC = UIAlertController(title: "提示", message: "游戏还在开发中,敬请期待", preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "确定", style: .destructive)
+            alertVC.addAction(confirmAction)
+            present(alertVC, animated: true)
+            return
+        }
+    }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameNameList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "HorsMiniGameTest")
+        cell.textLabel?.text = gameNameList[indexPath.row]
+        return cell
     }
 }
 
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        onSelectGame(index: indexPath.row)
+    }
+}
--- minigame/ZegoGameManager.swift
+++ minigame/ZegoGameManager.swift
@@ -6,7 +6,45 @@
 //
 
 import UIKit
+import Alamofire
+import ZegoMiniGameEngine
+
 
 class ZegoGameManager: NSObject {
-
+    let game_host = "http://192.168.30.211:3020"
+    
+    static let shared = ZegoGameManager()
+    
+    public func queryGameCoinWith(userID: String, appID: String, gameID: String, completion: (_ errorCode: Int, _ coin: Int)->Void) {
+        
+    }
+    
+    public func exchangeCoinWith(userID: String, appID: String, gameID: String, gameCoins: Int, completion: (_ errooCode: Int, _ coins: Int)->Void) {
+        
+    }
+    
+    public func getGameTokenWithUserID(userID: String, appID: String, completion: (_ errorCode:Int, _ token: String)->Void) {
+        
+    }
+    
+    public func describeUserCurrency(userID: String, gameID: String, completion: (_ errorCode: Int, _ token: String)->Void) {
+        
+    }
+    
+    public func getGameLauchCode(userID: String, appID: String, gameID: String, roomID: String, avatar: String, sex: Int, completion: (_ errorCode: Int, _ gameLaunchCode: String)->Void) {
+        
+    }
+    
+    public func postToURL(url: String, isJson: Bool, param: Dictionary<String, Any>, completion: (_ errorCode: Int, _ result: Result<Any, Error>)->Void) {
+        
+        
+    }
+    
+    func getCurrentTimeStamp()->String {
+        let date = Date(timeIntervalSinceNow: 0)
+        let interval = date.timeIntervalSince1970 * 1000
+        let stampString = "\(interval)"
+        return stampString
+    }
+    
 }
